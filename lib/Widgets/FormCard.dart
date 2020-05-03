8bfbd4464bba0e1de8a79433736ab03c2151267b
lovsokheang <lovsokheang@gmail.com>
2020-05-03 20:53:40 +0700

    First init

---
+++ lib/Widgets/FormCard.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FormCard extends StatelessWidget {
+  FormCard({Key key, this.param}) : super(key: key);
+  final Map param;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(bottom: 1),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0.0, 15.0),
+              blurRadius: 15.0
+            ),
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0.0, -10.0),
+              blurRadius: 10.0
+            ),
+          ]),
+      child: Padding(
+        padding: EdgeInsets.only(top: 25, right: 15, bottom: 25, left: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(param.isNotEmpty && param.containsKey('type') && param['type'] == 'login' ? 'Login' : 'Register',
+              style: TextStyle(
+                fontSize: ScreenUtil.getInstance().setSp(45),
+                fontFamily: "Poppins-Bold",
+                letterSpacing: .6
+              )
+            ),
+            SizedBox(
+              height: ScreenUtil.getInstance().setHeight(30),
+            ),
+            if (param.isNotEmpty && param.containsKey('type') && param['type'] != 'login')
+              Text("Name",
+                style: TextStyle(
+                  fontFamily: "Poppins-Medium",
+                  fontSize: ScreenUtil.getInstance().setSp(26)
+                )
+              ),
+            if (param.isNotEmpty && param.containsKey('type') && param['type'] != 'login')
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter name",
+                  hintStyle: TextStyle(color: Colors.grey, fontSize: 12.0)
+                ),
+              ),
+            if (param.isNotEmpty && param.containsKey('type') && param['type'] != 'login')
+              SizedBox(
+                height: ScreenUtil.getInstance().setHeight(30),
+              ),
+            Text("Email",
+              style: TextStyle(
+                fontFamily: "Poppins-Medium",
+                fontSize: ScreenUtil.getInstance().setSp(26)
+              )
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter email",
+                hintStyle: TextStyle(color: Colors.grey, fontSize: 12.0)
+              ),
+            ),
+            SizedBox(
+              height: ScreenUtil.getInstance().setHeight(30),
+            ),
+            Text("Password",
+              style: TextStyle(
+                fontFamily: "Poppins-Medium",
+                fontSize: ScreenUtil.getInstance().setSp(26)
+              )
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter password",
+                hintStyle: TextStyle(color: Colors.grey, fontSize: 12.0)
+              ),
+            ),
+            SizedBox(
+              height: ScreenUtil.getInstance().setHeight(35),
+            ),
+            if (param.isNotEmpty && param.containsKey('type') && param['type'] != 'login')
+              Text("Confirm Password",
+                style: TextStyle(
+                  fontFamily: "Poppins-Medium",
+                  fontSize: ScreenUtil.getInstance().setSp(26)
+                )
+              ),
+            if (param.isNotEmpty && param.containsKey('type') && param['type'] != 'login')
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter confirm password",
+                  hintStyle: TextStyle(color: Colors.grey, fontSize: 12.0)
+                )
+              ),
+            if (param.isNotEmpty && param.containsKey('type') && param['type'] != 'login')
+              SizedBox(
+                height: ScreenUtil.getInstance().setHeight(35),
+              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontFamily: "Poppins-Medium",
+                    fontSize: ScreenUtil.getInstance().setSp(28)
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'package:bedtimestories/main.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'Widgets/FormCard.dart';
+import 'Widgets/SocialIcons.dart';
+import 'CustomIcons.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<RegisterPage> {
+  bool _isSelected = false;
+
+  void _radio() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
+  Widget radioButton(bool isSelected) => Container(
+    width: 16.0,
+    height: 16.0,
+    padding: EdgeInsets.all(2.0),
+    decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(width: 2.0, color: Colors.black)),
+    child: isSelected
+        ? Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration:
+      BoxDecoration(shape: BoxShape.circle, color: Colors.black),
+    )
+        : Container(),
+  );
+
+  Widget horizontalLine() => Padding(
+    padding: EdgeInsets.symmetric(horizontal: 16.0),
+    child: Container(
+      width: ScreenUtil.getInstance().setWidth(120),
+      height: 1.0,
+      color: Colors.black26.withOpacity(.2),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334, allowFontScaling: true);
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomPadding: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Image.asset("assets/image_01.png"),
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              Expanded(child: Image.asset("assets/image_02.png"))
+            ],
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Image.asset(
+                        "assets/logo.png",
+                        width: ScreenUtil.getInstance().setWidth(110),
+                        height: ScreenUtil.getInstance().setHeight(110),
+                      ),
+                      Text("Stories",
+                        style: TextStyle(
+                          fontFamily: "Poppins-Bold",
+                          fontSize: ScreenUtil.getInstance().setSp(46),
+                          letterSpacing: .6,
+                          fontWeight: FontWeight.bold
+                        )
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(180),
+                  ),
+                  FormCard(param: {
+                    'type': 'register'
+                  }),
+                  SizedBox(height: ScreenUtil.getInstance().setHeight(40)),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      InkWell(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.86,
+                          height: ScreenUtil.getInstance().setHeight(100),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [
+                              Color(0xFF17ead9),
+                              Color(0xFF6078ea)
+                            ]),
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color(0xFF6078ea).withOpacity(.3),
+                                offset: Offset(0.0, 8.0),
+                                blurRadius: 8.0
+                              )
+                            ]
+                          ),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              onTap: () {},
+                              child: Center(
+                                child: Text("REGISTER",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: "Poppins-Bold",
+                                    fontSize: 18,
+                                    letterSpacing: 1.0
+                                  )
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(40),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      horizontalLine(),
+                      Text("Social Login",
+                        style: TextStyle(
+                          fontSize: 16.0, fontFamily: "Poppins-Medium"
+                        )
+                      ),
+                      horizontalLine()
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(40),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SocialIcon(
+                        colors: [
+                          Color(0xFF102397),
+                          Color(0xFF187adf),
+                          Color(0xFF00eaf8),
+                        ],
+                        iconData: CustomIcons.facebook,
+                        onPressed: () {},
+                      ),
+                      SocialIcon(
+                        colors: [
+                          Color(0xFFff4f38),
+                          Color(0xFFff355d),
+                        ],
+                        iconData: CustomIcons.googlePlus,
+                        onPressed: () {},
+                      ),
+                      SocialIcon(
+                        colors: [
+                          Color(0xFF17ead9),
+                          Color(0xFF6078ea),
+                        ],
+                        iconData: CustomIcons.twitter,
+                        onPressed: () {},
+                      ),
+                      SocialIcon(
+                        colors: [
+                          Color(0xFF00c6fb),
+                          Color(0xFF005bea),
+                        ],
+                        iconData: CustomIcons.linkedin,
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(30),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Already have an account? ",
+                        style: TextStyle(fontFamily: "Poppins-Medium"),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                              builder: (BuildContext context) => MyApp()
+                            ),
+                            (Route<dynamic> route) => false
+                          );
+                        },
+                        child: Text("Login",
+                          style: TextStyle(
+                            color: Color(0xFF5d74e3),
+                            fontFamily: "Poppins-Bold"
+                          )
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(20),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
